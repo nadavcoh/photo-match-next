ae702c432ffee9b03a3b98099044ab33e28ff583
Nadac Cohen <cohen.n@gmail.com>
2026-06-10 10:08:24 +0300

    10

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -12,10 +12,8 @@
  */
 
 import { NextRequest, NextResponse } from 'next/server'
-import { createServerClient, type CookieMethodsServer } from '@supabase/ssr'
+import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-
-type CookieToSet = Parameters<CookieMethodsServer['setAll']>[0][number]
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const { searchParams, origin } = new URL(request.url)
